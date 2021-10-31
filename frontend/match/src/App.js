b66ab34f6943880f513c4a3dab02e206283e4c07
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-10-31 11:29:42 -0500

    randomizing cards

--- frontend/match/src/App.js
+++ frontend/match/src/App.js
@@ -1,115 +1,17 @@
 import "./App.css";
 import Images from "./Images";
+import { useState } from "react";
 
 function App() {
+  const [cards, setCards] = useState(initialState, [...Images]);
   return (
-    <div className='board'>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
-        </div>
-      </div>
-      <div className='card-outer'>
-        <div className='card'>
-          <div className='front'></div>
-          <back></back>
+    <div>
+      <div className='board'>
+        <div className='card-outer'>
+          <div className='card'>
+            <div className='front'></div>
+            <div className='back'></div>
+          </div>
         </div>
       </div>
     </div>
