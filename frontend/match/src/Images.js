9b55b22a792c3e4fdf46ab2e277fbf924896303f
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-10-31 09:58:58 -0500

    importing and esporting card images

---
+++ frontend/match/src/Images.js
@@ -0,0 +1,23 @@
+import AxelCard from "./cards/AxelCard.png";
+import KHCardBack from "./cards/KHCardBack.png";
+import LarxeneCard from "./cards/LarxeneCardFront.png";
+import LexaeusCard from "./cards/LexauesCardFront.png";
+import MarluxiaCard from "./cards/MarluxiaCardFront.png";
+import NaminaCard from "./cards/NaminaCard.png";
+import RikuCard from "./cards/RikuCardFront.png";
+import SoraCard from "./cards/SoraCardFront.png";
+import VexenCard from "./cards/VexenCardFront.png";
+import ZexionCard from "./cards/ZexionCardFront.png";
+
+export default [
+  AxelCard,
+  KHCardBack,
+  LarxeneCard,
+  LexaeusCard,
+  MarluxiaCard,
+  NaminaCard,
+  RikuCard,
+  SoraCard,
+  VexenCard,
+  ZexionCard
+];
